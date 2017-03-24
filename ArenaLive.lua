af61ef42cb00ab40d9d6f7abff58f8df42076ded
Harald Böhm <harald@boehm.agency>
2017-03-24 16:22:52 +0100

    ArenaLive: Add basic addon setup functions.

--- ArenaLive.lua
+++ ArenaLive.lua
@@ -1,2 +1,37 @@
+assert(DeliUnitFrames, "ArenaLive requires DeliUnitFrames.");
+
 local addonName, L = ...;
 local ArenaLive = ArenaLive;
+
+--[[**
+  * Initializes the addon's base frame, setting script handlers and
+  * registering all necessary events.
+]]
+function ArenaLive:init()
+  self:SetScript("OnEvent", self.onEvent);
+  self:RegisterEvent("ADDON_LOADED");
+
+end
+
+--[[**
+  * OnEvent script handler for ArenaLive.
+  *
+  * @param event (string) the name of the event that fired.
+  * @param ... (mixed) a arbitrary number of arguments accompanying
+  * the event.
+]]
+function ArenaLive:onEvent(event, ...)
+  local arg1 = ...;
+  if (event == "ADDON_LOADED" and arg1 == addonName) then
+    self:onAddonLoaded();
+  end
+end
+
+--[[**
+  * Sets up ArenaLive after its saved variables have been loaded by
+  * the World of Warcraft client.
+]]
+function ArenaLive:onAddonLoaded()
+end
+
+ArenaLive:init();
