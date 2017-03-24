2e2feefaf5eac859b69b56984f9a7f1b09e52564
Harald Böhm <harald@boehm.agency>
2017-03-24 17:31:39 +0100

    Add class ArenaLiveUnitFrame.

---
+++ ArenaLiveUnitFrame.lua
@@ -0,0 +1,26 @@
+local UnitFrame = DeliUnitFrames:newClass("ArenaLiveUnitFrame");
+
+--[[**
+  * @Override
+  *
+  * @see AbstractUnitFrame
+]]
+function UnitFrame:getDatabase()
+  return ArenaLive.db;
+end
+
+--[[**
+  * @Override
+  * Updates this unit frame's appearance according to its settings.
+]]
+function UnitFrame:updateAppearance()
+  self:getSuper().updateAppearance(self);
+  local settings = self:getSettings();
+  local anchor = settings.anchor;
+
+  --[[
+    * We have to reanchor frames, since DeliUnitFrames currently
+    * anchors all frames with the same group at the exact same
+    * position.
+    ]]
+end
