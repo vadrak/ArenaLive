4ce80003d1ae7892547b5c3965928fd2dd781bcf
Harald Böhm <harald@boehm.agency>
2017-05-27 18:56:47 +0200

    UnitFrame: Set name text's default color mode to class colored.

--- defaults.lua
+++ defaults.lua
@@ -117,6 +117,7 @@ ArenaLive.defaults = {
           hideTradeSkills = true,
         },
         NameText = {
+          colorMode = DeliUnitFrames.classes.NameText.ColorModes.CLASS,
           enabled = true,
           realmMode = DeliUnitFrames.classes.NameText.RealmModes.NONE,
           anchor = {
@@ -235,6 +236,7 @@ ArenaLive.defaults = {
           hideTradeSkills = true,
         },
         NameText = {
+          colorMode = DeliUnitFrames.classes.NameText.ColorModes.CLASS,
           enabled = true,
           realmMode = DeliUnitFrames.classes.NameText.RealmModes.NONE,
           anchor = {
